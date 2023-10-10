b1953125b68264a592431560d92aee85d7cb2429
clement-stackup <120361535+clement-stackup@users.noreply.github.com>
2023-10-10 10:13:53 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/:path*", // this applies the headers to all routes. Adjust if needed.
-        headers: [
-          {
-            key: "Cross-Origin-Opener-Policy",
-            value: "same-origin", // set this to whatever value you want.
-          },
-        ],
-      },
-    ];
-  },
-};
+module.exports = {};
